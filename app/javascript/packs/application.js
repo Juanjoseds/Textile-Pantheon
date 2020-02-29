052701907e1887a264de34ca65fd3e0da22f297f
Juan <diazsantanajuanjose@gmail.com>
2020-02-29 19:59:29 +0000

    Bug fixes with bootstrap gems

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@ require("@rails/ujs").start()
 require("turbolinks")
 require("@rails/activestorage").start()
 require("channels")
-import 'bootstrap'
+require("bootstrap/dist/js/bootstrap")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
